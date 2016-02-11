909aeef446f22f1acba4d0c905e52c2b5e8a00b8
bradhave94 <bradleyhaveman@yahoo.ca>
2016-02-11 14:53:09 -0500

    Added post functionality to registration. Validates inputs.

--- build/routes/users.js
+++ build/routes/users.js
@@ -12,7 +12,11 @@ router.get('/', function(req, res, next) {
 
 router.get('/register', function(req, res, next) {
   res.render('register', {
-	'title': 'Register'
+	'title': 'Register',
+    'first': '',
+    'last': '',
+    'username': '',
+    'email': ''
   });
 });
 
@@ -20,15 +24,75 @@ router.post('/register', function(req, res, next) {
     if(!req.body) {
         return res.sendStatus(400);
     }
-    console.log(req.body.username);
+    //Store form variables
+    var first = req.body.firstName;
+    var last = req.body.lastName;
+    var username = req.body.username;
+    var email = req.body.email;
+
+    //Custom form validation
+    req.checkBody({
+        'firstName': {
+            notEmpty: true,
+                errorMessage: 'First name field is required'
+        },
+        'lastName': {
+            notEmpty: true,
+            errorMessage: 'Last name field is required'
+        },
+        'email': {
+            notEmpty: true,
+            isEmail: {
+                errorMessage: 'Invalid Email'
+            }
+        },
+        'username': {
+            notEmpty: true,
+            isLength: {
+                options: [{min: 7, max: 7}],
+                errorMessage: 'T# required'
+            }
+            //TODO: /t[0-9]{6}/
+        }
+    });
+
+    //Check for errors
+    var errors = req.validationErrors();
+    if(errors) {
+        req.flash('error', 'Missing required fields');
+        res.render('register', {
+            errors: errors,
+            first: first,
+            last: last,
+            email: email,
+            username: username
+        });
+    }
+    else {
+        var user = {
+          first_name: first,
+          last_name: last,
+          email: email,
+          username: username
+        };
+
+        //Database insertion
+        connection.get().query('INSERT INTO user SET ?', user, function(err, result) {
+            if(err) {
+                throw err;
+            }
+
+            console.log("User added");
+        });
+        res.redirect('/users/login');
+    }
 });
 
 router.get('/login', function(req, res, next) {
-  res.render('login', {
-	'title': 'Login'
-  });
+    res.render('login', {
+        'title': 'Login'
+    });
 });
-
 router.post('/login', passport.authenticate('local', {
     failureRedirect: '/users/login',
     failureFlash: 'Invalid username or password'
