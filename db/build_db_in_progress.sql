147819a10b480f5d9dac7237426e24fa57cd98fd
Michael <personal.michaelmorgan@gmail.com>
2016-02-15 11:16:16 -0500

    Updated sql files to work with what is currently there.

--- db/build_db_in_progress.sql
+++ db/build_db_in_progress.sql
@@ -1,34 +1,5 @@
--- Table structure for table stores
-CREATE TABLE stores
-(
-store_id varchar(4) NOT NULL,
-store_name varchar(255) NOT NULL,
-store_region varchar(5) NOT NULL,
-PRIMARY KEY (store_id)
-);
-
-
-
-
--- Table structure for table stats
-CREATE TABLE stats
-(
-stat_id int(50) NOT NULL AUTO_INCREMENT,
-t_number int(7) NOT NULL,
-store_id varchar(4) NOT NULL,
-controllable_transactions varchar(255) NOT NULL,
-actual int(10) NOT NULL,
-goal int(10) NOT NULL, 
-revenue int(10) NOT NULL,
-warranty int(10) NOT NULL,
-date_added date NOT NULL,
-PRIMARY KEY (stat_id),
-FOREIGN KEY (t_number) REFERENCES users(t_number),
-FOREIGN KEY (store_id) REFERENCES stores(store_id)
-);
-
-
-
+CREATE DATABASE build_db;
+USE build_db;
 
 -- Table structure for table behaviours
 CREATE TABLE behaviours
@@ -38,25 +9,14 @@ behaviour_desc varchar(255) NOT NULL,
 PRIMARY KEY (behaviour_id)
 );
 
-
-
-
--- Table structure for table observations
-CREATE TABLE observations
+-- Table structure for table stores
+CREATE TABLE stores
 (
-observation_id int(50) NOT NULL AUTO_INCREMENT,
-behaviour_id int(50) NOT NULL,
-t_number int(7) NOT NULL,
-observation_date date NOT NULL,
-observation_type tinyint(1) NOT NULL,
-observation_comment varchar(100),
-PRIMARY KEY (observation_id),
-FOREIGN KEY (t_number) REFERENCES users(t_number),
-FOREIGN KEY (behaviour_id) REFERENCES behaviours(behaviour_id)
+store_id varchar(4) NOT NULL,
+store_name varchar(255) NOT NULL,
+store_region varchar(5) NOT NULL,
+PRIMARY KEY (store_id)
 );
-
-
-
 
 -- Table structure for table users
 CREATE TABLE users
@@ -71,4 +31,35 @@ CREATE TABLE users
   store_id varchar(4) NOT NULL,
   PRIMARY KEY (t_number),
   FOREIGN KEY (store_id) REFERENCES stores(store_id)
+);
+
+-- Table structure for table observations
+CREATE TABLE observations
+(
+observation_id int(50) NOT NULL AUTO_INCREMENT,
+behaviour_id int(50) NOT NULL,
+t_number varchar(7) NOT NULL,
+observation_date date NOT NULL,
+observation_type tinyint(1) NOT NULL,
+observation_comment varchar(100),
+PRIMARY KEY (observation_id),
+FOREIGN KEY (t_number) REFERENCES users(t_number),
+FOREIGN KEY (behaviour_id) REFERENCES behaviours(behaviour_id)
+);
+
+-- Table structure for table stats
+CREATE TABLE stats
+(
+stat_id int(50) NOT NULL AUTO_INCREMENT,
+t_number varchar(7) NOT NULL,
+store_id varchar(4) NOT NULL,
+controllable_transactions varchar(255) NOT NULL,
+actual int(10) NOT NULL,
+goal int(10) NOT NULL, 
+revenue int(10) NOT NULL,
+warranty int(10) NOT NULL,
+date_added date NOT NULL,
+PRIMARY KEY (stat_id),
+FOREIGN KEY (t_number) REFERENCES users(t_number),
+FOREIGN KEY (store_id) REFERENCES stores(store_id)
 );
--- db/insert_dummy_records_in_progress.sql
+++ db/insert_dummy_records_in_progress.sql
@@ -1,30 +1,21 @@
--- Dumping data for table stores
-INSERT INTO stores (store_id, store_name, store_region) VALUES
-('6529','Georgian Mall', 'GNO');
-
-
-
--- Dumping data for table users
-INSERT INTO users (t_number, username, `password`, email, first_name, last_name, privileged, store_id) VALUES
-('t901159', 'T901159', 'password', 'patrick.richey@telus.com', 'Patrick', 'Richey', 1, '6529'),
-('t901159', 'T901159', 'password', 'patrick.richey@telus.com', 'Patrick', 'Richey', 1, '6529'),
-('t901159', 'T901159', 'password', 'patrick.richey@telus.com', 'Patrick', 'Richey', 1, '6529'),
-('t901159', 'T901159', 'password', 'patrick.richey@telus.com', 'Patrick', 'Richey', 1, '6529');
-
-
-
--- Dumping data for table stats
-INSERT INTO stats (t_number, store_id, controllable_transactions, actual, goal, revenue, warranty, date_added) VALUES
-('t901159','6529', 'GNO');
-
-
-
 -- Dumping data for table behaviours
 INSERT INTO behaviours (behaviour_desc)
 VALUES ('Greeting clients'), ('Top-down selling'), ('Another one'), ('Another one');
 
+-- Dumping data for table stores
+INSERT INTO stores (store_id, store_name, store_region) VALUES
+('6529','Georgian Mall', 'GNO');
 
+-- Dumping data for table users
+INSERT INTO users (t_number, username, `password`, email, first_name, last_name, privileged, store_id) VALUES
+('t901159', 'T901159', 'password', 'patrick.richey@telus.com', 'Patrick', 'Richey', 1, '6529'),
+('t123456', 'T123456', 'password', 'michael.morgan@telus.com', 'Michael', 'Morgan', 1, '6529');
+
+-- Dumping data for table stats
+INSERT INTO stats (t_number, store_id, controllable_transactions, actual, goal, revenue, warranty, date_added) VALUES
+('t901159','6529', 'GNO', 1, 1, 1, 1, '016-02-01'),
+('t123456','6529', 'GNO', 1, 1, 1, 1, '016-02-01');
 
 -- Dumping data for table observations
 INSERT INTO observations (behaviour_id, t_number, observation_date, observation_type, observation_comment)
-VALUES ('0', 't901159', '2016-02-01', 1, 'Employee greeted all customers as they entered the store'), ('0', 't901159', '2016-02-01', 1, 'Employee greeted all customers as they entered the store');
+VALUES (1, 't901159', '2016-02-01', 1, 'Employee greeted all customers as they entered the store'), (1, 't901159', '2016-02-01', 1, 'Employee greeted all customers as they entered the store');
