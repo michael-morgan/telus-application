c25b3ff511e49e8cb9a57788b7059796a8553c2d
j-whiteside <whiteside.jeremy@gmail.com>
2016-03-20 12:59:40 -0400

    Greyed out submit button on add-obs page

--- build/public/javascripts/main.js
+++ build/public/javascripts/main.js
@@ -5,6 +5,7 @@ $(function() {
     $( ".unchecked" ).click(function() {
         $( ".btn").removeClass('active')
         $("#submitObservation").removeClass('disabled')
+        $("#submitObservation").addClass('btn-success')
         var submitButton = document.getElementById("submitObservation").type = "submit";
     });
 });
