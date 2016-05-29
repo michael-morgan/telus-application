fde38c89bbe78e16e251214b221d35ffeb215b77
Jake0303 <jake0303@hotmail.ca>
2016-05-29 19:49:30 -0400

    Add observation page is prepopulated with data

--- build/routes/users/behaviours.js
+++ build/routes/users/behaviours.js
@@ -34,7 +34,7 @@ router.get('/', ensureAuthenticated, function (req, res, next) {
         }
 
         //Connection to get all behaviours
-        connection.get().query('SELECT skill_id,behaviour_desc FROM behaviours', function (err, behaviourResults) {
+        connection.get().query('SELECT skill_id,behaviour_id,behaviour_desc FROM behaviours', function (err, behaviourResults) {
             //If an error is thrown
             if (err) {
                 returnObj['message'] = 'Our database servers maybe down. Please try again.';
@@ -56,18 +56,53 @@ router.get('/add-behaviour', ensureAuthenticated, function (req, res, next) {
         title: 'Add Skills/Behaviours'
     };
     //Connection to get all behaviours
-    connection.get().query('SELECT skill_id,behaviour_desc FROM behaviours', function (err, behaviourResults) {
+    connection.get().query('SELECT skill_id,skill_title FROM skills', function (err, skillResults) {
         //If an error is thrown
         if (err) {
             returnObj['message'] = 'Our database servers maybe down. Please try again.';
             //Render the page wth error messages
-    return res.render('add-behaviour', returnObj);
-            return res.render('behaviours', returnObj);
+            return res.render('add-behaviour', returnObj);
         }
 
         returnObj['skills'] = skillResults;
         //Render the observations page with the list of users and observations
         return res.render('add-behaviour', returnObj);
+    });
+});
+
+// When the add-observation page is loaded, and there is a t_number is the url. Render the add observations page
+router.get('/add-behaviour/:behaviour', ensureAuthenticated, function (req, res, next) {
+    if (!req.body) { return res.sendStatus(400); }
+
+    var returnObj = {
+        title: 'Add Skills/Behaviours'
+    };
+    //Connection to get all behaviours
+    connection.get().query('SELECT skill_id,skill_title FROM skills', function (err, skillResults) {
+        //If an error is thrown
+        if (err) {
+            returnObj['message'] = 'Our database servers maybe down. Please try again.';
+            //Render the page wth error messages
+            return res.render('add-behaviour', returnObj);
+        }
+        else {
+            connection.get().query('SELECT skills.skill_id,skills.skill_title,behaviour_desc FROM skills INNER JOIN behaviours ON skills.skill_id = behaviours.skill_id ' +
+                'WHERE behaviours.behaviour_id = ?', req.params.behaviour, function (err, behaviourResults) {
+                //If an error is thrown
+                if (err) {
+                    returnObj['message'] = 'Our database servers maybe down. Please try again.';
+                    //Render the page wth error messages
+                    return res.render('add-behaviour', returnObj);
+                }
+                else {
+                    returnObj['skills'] = skillResults;
+                    returnObj['selectedbehaviour'] = req.params.behaviour;
+                    returnObj['selectedskill'] = behaviourResults;
+                    //Render the observations page with the list of users and observations
+                    return res.render('add-behaviour', returnObj);
+                }
+            });
+        }
     });
 });
 // Ensure sure the user is authenticated
