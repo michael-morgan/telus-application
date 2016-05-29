79da3f883908de92ca5fdb1c0af50d1794d7944b
Jake0303 <jake0303@hotmail.ca>
2016-05-29 17:16:13 -0400

    Displayed all skills and behaviours

--- build/routes/users/behaviours.js
+++ build/routes/users/behaviours.js
@@ -1,52 +1,78 @@
+/**
+ * Created by Jacob on 2016-05-29.
+ */
 var express = require('express');
 var connection = require('../../connection');
 var passport = require('passport');
 
-var router = express.Router();
+var behaviourModel = require('../../models/observation');
 
-// Get for observations when the page is loaded, show the observations for each user
+var router = express.Router();
+// Get for behaviours and show them for each skill
 router.get('/', ensureAuthenticated, function (req, res, next) {
     if (!req.body) { return res.sendStatus(400); }
 
     var returnObj = {
-        title: 'Behaviours',
-        behaviours: [{
-            id:1,
-            name:'Skill #1'
-        },{
-            id:2,
-            name:'Skill #2'
-        },{
-            id:3,
-            name:'Skill #3'
-        },{
-            id:4,
-            name:'Skill #4'
-        },{
-            id:5,
-            name:'Add New Skill'
-        },]
+        title: 'Behaviours'
     };
 
-    return res.render('behaviours', returnObj);
-});
-
-router.get('/add-behaviour', ensureAuthenticated, function (req, res, next) {
-    if (!req.body) {
-        return res.sendStatus(400);
+    //Display success message on adding observation
+    if(req.session.success)
+    {
+        req.flash('success_messages', 'Behaviour successfully added!');
+        res.locals.success_messages = req.flash('success_messages');
+        req.session.success = false;
     }
 
-    var returnObj = {
-        title: 'Add Behaviour'
-    };
+    //Connection to get all the skills
+    connection.get().query('SELECT skill_id,skill_title FROM skills', function (err, skillResults) {
+        //If an error is thrown
+        if (err) {
+            returnObj['message'] = 'Our database servers maybe down. Please try again.';
+            //Render the page wth error messages
+            return res.render('behaviours', returnObj);
+        }
 
-    return res.render('add-behaviour', returnObj);
+        //Connection to get all behaviours
+        connection.get().query('SELECT skill_id,behaviour_desc FROM behaviours', function (err, behaviourResults) {
+            //If an error is thrown
+            if (err) {
+                returnObj['message'] = 'Our database servers maybe down. Please try again.';
+                //Render the page wth error messages
+                return res.render('behaviours', returnObj);
+            }
+
+            returnObj['skills'] = skillResults;
+            returnObj['behaviours'] = behaviourResults;
+            //Render the observations page with the list of users and observations
+            res.render('behaviours', returnObj);
+        });
+    });
 });
+router.get('/add-behaviour', ensureAuthenticated, function (req, res, next) {
+    if (!req.body) { return res.sendStatus(400); }
 
+    var returnObj = {
+        title: 'Add Skills/Behaviours'
+    };
+    //Connection to get all behaviours
+    connection.get().query('SELECT skill_id,behaviour_desc FROM behaviours', function (err, behaviourResults) {
+        //If an error is thrown
+        if (err) {
+            returnObj['message'] = 'Our database servers maybe down. Please try again.';
+            //Render the page wth error messages
+    return res.render('add-behaviour', returnObj);
+            return res.render('behaviours', returnObj);
+        }
+
+        returnObj['skills'] = skillResults;
+        //Render the observations page with the list of users and observations
+        return res.render('add-behaviour', returnObj);
+    });
+});
 // Ensure sure the user is authenticated
 function ensureAuthenticated(req, res, next) {
     if (req.isAuthenticated()) { return next(); }
     res.redirect('/');
 }
-
 module.exports = router;
