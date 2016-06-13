b261f17c60670933ce89b96a7446de43531bca3f
bennyva <benjaminvanarragon@gmail.com>
2016-06-13 12:29:17 -0400

    Added transactions table for tracking EPS and WPS stats per user per day, as well as multiple tables to populate dropdowns for webapp

--- db/build_db.sql
+++ db/build_db.sql
@@ -174,10 +174,9 @@ CREATE TABLE `stats` (
 --
 -- Dumping data for table `stats`
 --
-
 LOCK TABLES `stats` WRITE;
 /*!40000 ALTER TABLE `stats` DISABLE KEYS */;
-INSERT INTO `stats` VALUES (1,'t901159','6529','GNO',1,1,1,1,'0016-02-01'),(2,'t123456','6529','GNO',1,1,1,1,'0016-02-01');
+INSERT INTO `stats` VALUES (1,'t901159','6529','GNO',1,1,1,1,'0016-02-01'),(2,'t111111','6529','GNO',1,1,1,1,'0016-02-01');
 /*!40000 ALTER TABLE `stats` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -286,3 +285,370 @@ UNLOCK TABLES;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
 -- Dump completed on 2016-02-15 19:38:29users
+
+--
+-- Table structure for table `selling_hours_schedule`
+--
+
+DROP TABLE IF EXISTS `selling_hours_schedule`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `selling_hours_schedule` (
+	`hours_id` int(255) NOT NULL AUTO_INCREMENT,
+	`team_member` varchar(7) NOT NULL,
+    `date` date NOT NULL,
+	`store_id` varchar(4) NOT NULL,
+    `selling_hours` float, 
+    PRIMARY KEY (`hours_id`)
+)	ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `selling_hours_schedule`
+--
+
+LOCK TABLES `selling_hours_schedule` WRITE;
+/*!40000 ALTER TABLE `selling_hours_schedule` DISABLE KEYS */;
+INSERT INTO `selling_hours_schedule` VALUES (1,'t123456','0016-02-01', '6587', 5),(2,'t123456','0016-02-02', '6587', 6),(3,'t123456','0016-02-03', '6587', 4),(4,'t123456','0016-02-04', '6587', 9),(5,'t123456','0016-02-05', '6587', 3);
+/*!40000 ALTER TABLE `selling_hours_schedule` ENABLE KEYS */;
+UNLOCK TABLES;
+
+-- select * from selling_hours_schedule
+
+
+--
+-- Table structure for table `discount_tracking`
+--
+
+DROP TABLE IF EXISTS `discount_tracking`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `discount_tracking` (
+	
+	`team_member` varchar(7) NOT NULL,
+    `pocket25` int(255) NOT NULL,
+    `pocket50` int(255) NOT NULL,
+    `pocket75` int(255) NOT NULL,
+    `pocket100` int(255) NOT NULL,
+    `inc25` int(255) NOT NULL,
+    `inc50` int(255) NOT NULL,
+	`store_id` varchar(4) NOT NULL,
+    PRIMARY KEY (`team_member`)
+)	ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `selling_hours_schedule`
+--
+
+LOCK TABLES `discount_tracking` WRITE;
+/*!40000 ALTER TABLE `discount_tracking` DISABLE KEYS */;
+INSERT INTO `discount_tracking` VALUES ('t123456',25,25,25,25,50,50,'6587'),('t111111',25,25,25,25,50,50,'6587');
+/*!40000 ALTER TABLE `discount_tracking` ENABLE KEYS */;
+UNLOCK TABLES;
+
+-- SELECT * FROM discount_tracking
+
+
+--
+-- Table structure for table `discount_tracking`
+--
+
+DROP TABLE IF EXISTS `results_to_send`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `results_to_send` (
+	`result_id` int(255) NOT NULL AUTO_INCREMENT,
+	`cont_transcations` int(255) NOT NULL,
+    `cont_revenue` int(255) NOT NULL,
+    `connected_hs` int(255) NOT NULL,
+    `acc_units_per_transaction` int(255) NOT NULL,
+    `basket_size` int (255) NOT NULL,
+    `learning_sessions` int(255) NOT NULL,
+    `aotm` int(255) NOT NULL,
+    `telus_credit_card` int(255) NOT NULL,
+    `sbs_activations` int(255) NOT NULL,
+    `tablets` int(255) NOT NULL,
+    `warranty_percentage` float NOT NULL,
+    `backpocket` int(255) NOT NULL,
+    `inc_coupons` int(255) NOT NULL,
+    `date` date NOT NULL,
+	`store_id` varchar(4) NOT NULL,
+    PRIMARY KEY (`result_id`)
+)	ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `selling_hours_schedule`
+--
+
+LOCK TABLES `results_to_send` WRITE;
+/*!40000 ALTER TABLE `results_to_send` DISABLE KEYS */;
+INSERT INTO `results_to_send` VALUES (1,1,1,1,1,1,1,1,1,1,1,75,1,1,'0016-01-02','6587');
+/*!40000 ALTER TABLE `results_to_send` ENABLE KEYS */;
+UNLOCK TABLES;
+
+-- SELECT * FROM results_to_send
+
+--
+-- Table structure for table `wps_day`
+--
+
+DROP TABLE IF EXISTS `wps_day`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `wps_day` (
+	`wps_id` int(255) NOT NULL AUTO_INCREMENT,
+	`num_of_transactions` int(255) NOT NULL,
+    `controllable_transactions` int(255) NOT NULL,
+    `controllable_revenue` int(255) NOT NULL,
+    `accessory_units_per_transaction` int(255) NOT NULL,
+    `basket_size` int (255) NOT NULL,
+    `learning_sessions` int(255) NOT NULL,
+    `learning_sessions_percentage_to_handsets` int(255) NOT NULL,
+    `aotm` int(255) NOT NULL,
+    `telus_credit_card` int(255) NOT NULL,
+    `sbs_activations` int(255) NOT NULL,
+    `tablets` int(255) NOT NULL,
+    `device_care_warranty_percentage` float NOT NULL,
+    `apple_care_warranty_percentage` float NOT NULL,
+    `overall_warranty_percentage` float NOT NULL,
+    `num_of_devices` int(255) NOT NULL,
+    `num_of_iphone` int(255) NOT NULL,
+    `num_of_android` int(255),
+    `num_of_others` int(255) NOT NULL,
+    `backpocket` int(255) NOT NULL,
+    `inc_coupons` int(255) NOT NULL,
+    `critters` int(255) NOT NULL,
+    `donations` float NOT NULL,
+    `date` date NOT NULL,
+	`store_id` varchar(4) NOT NULL,
+    PRIMARY KEY (`wps_id`)
+)	ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `selling_hours_schedule`
+--
+
+LOCK TABLES `wps_day` WRITE;
+/*!40000 ALTER TABLE `wps_day` DISABLE KEYS */;
+INSERT INTO `wps_day` VALUES (1,1,1,1,1,1,1,1,1,1,1,1,75,76,77,1,1,1,1,1,1,1,65,'0016-01-02','6587');
+/*!40000 ALTER TABLE `wps_day` ENABLE KEYS */;
+UNLOCK TABLES;
+
+-- select * FROM wps_day;
+
+
+
+
+
+
+--
+-- table schema for transactions
+
+DROP TABLE IF EXISTS `transactions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `transactions` (
+  `transaction_id` int(255) NOT NULL AUTO_INCREMENT,
+  `t_number` varchar(7) NOT NULL,
+  `store_id` varchar(4) NOT NULL,
+  `transaction_date` datetime NOT NULL,
+  `transaction_type` varchar(50) NOT NULL,
+  `activation_type` varchar(50) NOT NULL,
+  `device_type` varchar (50) NOT NULL,
+  `warranty_type` varchar(50) NOT NULL,
+  `attachments` bit NOT NULL,
+  `revenue` float NOT NULL,
+  `number_of_accessories` int(255) NOT NULL,
+  `sbs_return_exchange_discount` varchar(50) NOT NULL,
+  `learning_sessions_aotm_appointments` varchar(50) NOT NULL,
+  `critters_donations_creditcard` varchar(50) NOT NULL,
+  PRIMARY KEY (`transaction_id`),
+  KEY `t_number` (`t_number`),
+  KEY `store_id` (`store_id`),
+  CONSTRAINT `transactions_tfk_1` FOREIGN KEY (`t_number`) REFERENCES `users` (`t_number`),
+  CONSTRAINT `transactions_tfk_2` FOREIGN KEY (`store_id`) REFERENCES `stores` (`store_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `stats`
+--
+USE build_db;
+LOCK TABLES `transactions` WRITE;
+/*!40000 ALTER TABLE `transactions` DISABLE KEYS */;
+INSERT INTO `transactions` VALUES (1,'t111111','6529','0016-02-01', 'Device', 'Renewal', 'Android','Device Care +', 1, 750.00, 4, 'Not Applicable', 'Not Applicable','Not Applicable'); 
+/*!40000 ALTER TABLE `transactions` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+-- select * from transactions;
+
+
+--
+-- table for transaction types
+--
+DROP TABLE IF EXISTS `transaction_types`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `transaction_types` (
+	`transaction_type_id` int(255) NOT NULL AUTO_INCREMENT,
+	`transaction_types` varchar(255) NOT NULL,
+    PRIMARY KEY (`transaction_type_id`)
+)	ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `selling_hours_schedule`
+--
+
+LOCK TABLES `transaction_types` WRITE;
+/*!40000 ALTER TABLE `transaction_types` DISABLE KEYS */;
+INSERT INTO `transaction_types` VALUES (1,'Device'),(2,'Accessory'),(3,'Outright Sim'),(4,'VPIN'),(5,'Repair/Return'),(6,'Other Elligable Transaction');
+/*!40000 ALTER TABLE `transaction_types` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- table for activaiton types
+--
+DROP TABLE IF EXISTS `activation_types`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `activation_types` (
+	`activation_type_id` int(255) NOT NULL AUTO_INCREMENT,
+	`activation_types` varchar(255) NOT NULL,
+    PRIMARY KEY (`activation_type_id`)
+)	ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `selling_hours_schedule`
+--
+
+LOCK TABLES `activation_types` WRITE;
+/*!40000 ALTER TABLE `activation_types` DISABLE KEYS */;
+INSERT INTO `activation_types` VALUES (1,'New Activation'),(2,'Renewal'),(3,'Prepaid'),(4,'Outright');
+/*!40000 ALTER TABLE `activation_types` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- table for device types
+--
+DROP TABLE IF EXISTS `device_types`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `device_types` (
+	`device_type_id` int(255) NOT NULL AUTO_INCREMENT,
+	`device_types` varchar(255) NOT NULL,
+    PRIMARY KEY (`device_type_id`)
+)	ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `device_types`
+--
+
+LOCK TABLES `device_types` WRITE;
+/*!40000 ALTER TABLE `device_types` DISABLE KEYS */;
+INSERT INTO `device_types` VALUES (1,'iPhone'),(2,'Android'),(3,'Blackberry'),(4,'Other Device'),(5,'SIM'),(6,'Tablet'),(7,'iPad'),(8,'Mobile Internet');
+/*!40000 ALTER TABLE `device_types` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+--
+-- table for `warranty_types`
+--
+DROP TABLE IF EXISTS `warranty_types`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `warranty_types` (
+	`warranty_type_id` int(255) NOT NULL AUTO_INCREMENT,
+	`warranty_types` varchar(255) NOT NULL,
+    PRIMARY KEY (`warranty_type_id`)
+)	ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `warranty_types`
+--
+
+LOCK TABLES `warranty_types` WRITE;
+/*!40000 ALTER TABLE `warranty_types` DISABLE KEYS */;
+INSERT INTO `warranty_types` VALUES (1,'Device Care'),(2,'Apple Care +'),(3,'Device Care & T-UP'),(4,'Apple Care & T-UP');
+/*!40000 ALTER TABLE `warranty_types` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+--
+-- table for `SBS_returns_exchanges`
+--
+DROP TABLE IF EXISTS `sbs_returns_exchanges`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `sbs_returns_exchanges` (
+	`sbs_returns_exchanges_type_id` int(255) NOT NULL AUTO_INCREMENT,
+	`sbs_returns_exchanges_types` varchar(255) NOT NULL,
+    PRIMARY KEY (`sbs_returns_exchanges_type_id`)
+)	ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `SBS_returns_exchanges`
+--
+
+LOCK TABLES `sbs_returns_exchanges` WRITE;
+/*!40000 ALTER TABLE `sbs_returns_exchanges` DISABLE KEYS */;
+INSERT INTO `sbs_returns_exchanges` VALUES (1,'SBS Activations'),(2,'Returns'),(3,'Exchange'),(4,'Discounts');
+/*!40000 ALTER TABLE `sbs_returns_exchanges` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+--
+-- table for `learning_sessions_aotm_apts`
+--
+DROP TABLE IF EXISTS `learning_sessions_aotm_apts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `learning_sessions_aotm_apts` (
+	`learning_sessions_aotm_apts_type_id` int(255) NOT NULL AUTO_INCREMENT,
+	`learning_sessions_aotm_apts_types` varchar(255) NOT NULL,
+    PRIMARY KEY (`learning_sessions_aotm_apts_type_id`)
+)	ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `learning_sessions_aotm_apts`
+--
+
+LOCK TABLES `learning_sessions_aotm_apts` WRITE;
+/*!40000 ALTER TABLE `learning_sessions_aotm_apts` DISABLE KEYS */;
+INSERT INTO `learning_sessions_aotm_apts` VALUES (1,'Learning'),(2,'Sessions'),(3,'AOTM'),(4,'Appointments');
+/*!40000 ALTER TABLE `learning_sessions_aotm_apts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
+-- table for `critters_donations_creditcard`
+--
+DROP TABLE IF EXISTS `critters_donations_creditcard`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `critters_donations_creditcard` (
+	`critters_donations_creditcard_type_id` int(255) NOT NULL AUTO_INCREMENT,
+	`critters_donations_creditcard_types` varchar(255) NOT NULL,
+    PRIMARY KEY (`critters_donations_creditcard_type_id`)
+)	ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `critters_donations_creditcard`
+--
+
+LOCK TABLES `critters_donations_creditcard` WRITE;
+/*!40000 ALTER TABLE `critters_donations_creditcard` DISABLE KEYS */;
+INSERT INTO `critters_donations_creditcard` VALUES (1,'Critters'),(2,'Donations'),(3,'Credit Card');
+/*!40000 ALTER TABLE `critters_donations_creditcard` ENABLE KEYS */;
+UNLOCK TABLES;
+
+
