239685ab4c62e6917b00ea2cc947e79b62404542
Bradley Haveman <bradley@haveman.ca>
2016-06-20 21:03:26 -0400

    Added eps files and routes

--- build/app.js
+++ build/app.js
@@ -22,6 +22,7 @@ var users = require('./routes/users');
 var observations = require('./routes/users/observations');
 var behaviours = require('./routes/users/behaviours');
 var binder = require('./routes/users/binder');
+var eps = require('./routes/eps');
 var app = express();
 
 // view engine setup
@@ -120,6 +121,7 @@ app.use('/users', users);
 app.use('/users/observations', observations);
 app.use('/users/behaviours', behaviours);
 app.use('/users/binder', binder);
+app.use('/eps', eps);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
