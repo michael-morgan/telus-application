c0a551c677cbd924d534125f36f61c7c775092a1
SamuelHalloran <samuel_halloran@rogers.com>
2016-07-05 23:34:33 -0400

    Added date picker to Observation page + tweaks

--- build/public/javascripts/observations/observations.js
+++ build/public/javascripts/observations/observations.js
@@ -1,4 +1,13 @@
-$(function() {});
+$('input[name="dateRange"]').daterangepicker(
+    {
+        locale: {
+            format: 'MMMM D, YYYY'
+        }
+    },
+    function(start, end, label) {
+        alert("A new date range was chosen: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+    }
+);
 
 function deleteObservation(id) {
     alert('Are you sure you want to delete this observation?');
--- build/public/javascripts/transactions/transactions.js
+++ build/public/javascripts/transactions/transactions.js
@@ -1,8 +1,13 @@
-$(function() {
-    $('input[name="dateRange"]').daterangepicker();
-
-    console.debug(storeObj);
-});
+$('input[name="dateRange"]').daterangepicker(
+    {
+        locale: {
+            format: 'MMMM D, YYYY'
+        }
+    },
+    function(start, end, label) {
+        alert("A new date range was chosen: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+    }
+);
 
 function deleteTransaction(id) {
     alert('Are you sure you want to delete this transaction?');
