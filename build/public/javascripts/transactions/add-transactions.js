68d773b85560ff5aa77b83549fcc2a3b24c34b00
Bradley Haveman <bradley@haveman.ca>
2016-07-11 19:19:08 -0400

    Fixed bugs

--- build/public/javascripts/transactions/add-transactions.js
+++ build/public/javascripts/transactions/add-transactions.js
@@ -1,5 +1,5 @@
-$(function() {
-        loadButtons();
+$(function () {
+    loadButtons();
 });
 
 function loadButtons() {
@@ -78,31 +78,157 @@ function loadButtons() {
 
 var count = 2;
 function addDevices() {
-    var deviceFields = '<div class="single-device"><h3 class="device show-if-device">Device  ' + count + '</h3><div class="form-group show-if-device"><div class="col-sm-2 text-right transaction"><span class="transaction">Activation Type:</span></div><div class="col-sm-10"><select required="" id="activationDropdown'+count+'" name="activationDropdown'+count+'" class="transact"><option disabled="" selected="" value="0">Select Activation Type</option><option value="1">New Activation</option><option value="2">Renewal</option><option value="3">Prepaid</option><option value="4">Outright</option></select><div class="sbs"><input type="checkbox" name="sbsActivation' + count + '"><span>SBS Activation?</span></div></div></div><!--Show only if transaction type is device--><div class="form-group show-if-device"><div class="col-sm-2 text-right transaction"><span class="transaction">Device Type:</span></div><div class="col-sm-10"><select required="" id="deviceDropdown'+count+'" name="deviceDropdown'+count+'" class="transact"><option disabled="" selected="" value="0">Select Device Type</option><option value="1">iPhone</option><option value="2">Android</option><option value="3">Blackberry</option><option value="4">Other Device</option><option value="5">SIM</option><option value="6">Tablet</option><option value="7">iPad</option><option value="8">Mobile Internet</option></select></div></div><!--Show only if transaction type is device--><div class="form-group show-if-device"><div class="col-sm-2 text-right transaction"><span class="transaction">Warranty Type:</span></div><div class="col-sm-10"><select required="" id="warrantyDropdown'+count+'" name="warrantyDropdown'+count+'"><option disabled="" selected="" value="0" class="transact">Select Warranty Type</option><option value="1">Device Care</option><option value="2">Device Care &amp; T-UP</option><option value="3">AppleCare+</option><option value="4">AppleCare+ &amp; T-UP</option></select></div></div><!--Show only on all transaction types, but Accessory--><div class="form-group show-if-not-accessory"><div class="col-sm-2 text-right transaction"><span class="transaction">Attached?</span></div><div class="col-sm-10"><select required="" id="attached'+count+'" name="attachedDropdown'+count+'" class="transact"><option selected="" value="no">No</option><option value="yes">Yes</option></select></div></div><!--Show on all transaction types--><div class="form-group show-on-all"><div class="col-sm-2 text-right transaction"><span class="transaction">Number of Accessories:</div></span><div class="col-sm-10"><input type="number" name="accessoryCount'+count+'" value="0" min="0" max="100" class="form-control input-number"></div></div></div>';
+    //Add device fields
+    var deviceFields = '<div class="single-device" id="device'+count+' ">\
+                            <h3 class="device show-if-device">Device  ' + count + '</h3>\
+                            <div class="form-group show-if-device">\
+                                <div class="col-sm-2 text-right transaction"><span class="transaction">Activation Type:</span></div>\
+                                <div class="col-sm-10">\
+                                    <select required="" id="activationDropdown' + count + '" name="activationDropdown' + count + '" class="transact">\
+                                        <option disabled="" selected="" value="0">Select Activation Type</option>\
+                                        <option value="1">New Activation</option>\
+                                        <option value="2">Renewal</option>\
+                                        <option value="3">Prepaid</option>\
+                                        <option value="4">Outright</option>\
+                                    </select>\
+                                    <div class="sbs"><input type="checkbox" name="sbsActivation' + count + '"><span>SBS Activation?</span></div>\
+                                </div>\
+                            </div>\
+                            <!--Show only if transaction type is device-->\
+                            <div class="form-group show-if-device">\
+                                <div class="col-sm-2 text-right transaction"><span class="transaction">Device Type:</span></div>\
+                                <div class="col-sm-10">\
+                                    <select required="" id="deviceDropdown' + count + '" name="deviceDropdown' + count + '" class="transact">\
+                                        <option disabled="" selected="" value="0">Select Device Type</option>\
+                                        <option value="1">iPhone</option>\
+                                        <option value="2">Android</option>\
+                                        <option value="3">Blackberry</option>\
+                                        <option value="4">Other Device</option>\
+                                        <option value="5">SIM</option>\
+                                        <option value="6">Tablet</option>\
+                                        <option value="7">iPad</option>\
+                                        <option value="8">Mobile Internet</option>\
+                                    </select>\
+                                </div>\
+                            </div>\
+                            <!--Show only if transaction type is device-->\
+                            <div class="form-group show-if-device">\
+                                <div class="col-sm-2 text-right transaction"><span class="transaction">Warranty Type:</span></div>\
+                                <div class="col-sm-10">\
+                                    <select required="" id="warrantyDropdown' + count + '" name="warrantyDropdown' + count + '" class="warrantyDropdown">\
+                                        <option disabled="" selected="" value="0" class="transact">Select Warranty Type</option>\
+                                        <option value="1">Device Care</option>\
+                                        <option value="2">Device Care &amp; T-UP</option>\
+                                        <option value="3">AppleCare+</option>\
+                                        <option value="4">AppleCare+ &amp; T-UP</option>\
+                                    </select>\
+                                </div>\
+                            </div>\
+                            <!--Show only on all transaction types, but Accessory-->\
+                            <div class="form-group show-if-not-accessory">\
+                                <div class="col-sm-2 text-right transaction"><span class="transaction">Attached?</span></div>\
+                                <div class="col-sm-10">\
+                                    <select required="" id="attached' + count + '" name="attachedDropdown' + count + '" class="transact">\
+                                        <option selected="" value="no">No</option>\
+                                        <option value="yes">Yes</option>\
+                                    </select>\
+                                </div>\
+                            </div>\
+                            <!--Show on all transaction types-->\
+                            <div class="form-group show-on-all">\
+                                <div class="col-sm-2 text-right transaction"><span class="transaction">Number of Accessories:</div></span>\
+                                <div class="col-sm-10">\
+                                    <input type="number" name="accessoryCount' + count + '" value="0" min="0" max="100" class="form-control input-number">\
+                                </div>\
+                            </div>\
+                        </div>';
     $('.device-group').append(deviceFields);
     count++;
-    $('.show-if-device').attr('style','display: block');
+    $('.show-if-device').attr('style', 'display: block');
     $('.show-if-not-accessory').attr('style', 'display: block');
-    $('.show-on-all').attr('style','display: block');
+    $('.show-on-all').attr('style', 'display: block');
+
+
 }
 
 function resetDevice() {
-    var deviceFields = '<div class="single-device"><h3 class="device show-if-device">Device 1</h3><div class="form-group show-if-device"><div class="col-sm-2 text-right transaction"><span class="transaction">Activation Type:</span></div><div class="col-sm-10"><select required="" id="activationDropdown" name="activationDropdown" class="transact"><option disabled="" selected="" value="0">Select Activation Type</option><option value="1">New Activation</option><option value="2">Renewal</option><option value="3">Prepaid</option><option value="4">Outright</option></select><div class="sbs"><input type="checkbox" name="sbsActivation"><span>SBS Activation?</span></div></div></div><!--Show only if transaction type is device--><div class="form-group show-if-device"><div class="col-sm-2 text-right transaction"><span class="transaction">Device Type:</span></div><div class="col-sm-10"><select required="" id="deviceDropdown" name="deviceDropdown" class="transact"><option disabled="" selected="" value="0">Select Device Type</option><option value="1">iPhone</option><option value="2">Android</option><option value="3">Blackberry</option><option value="4">Other Device</option><option value="5">SIM</option><option value="6">Tablet</option><option value="7">iPad</option><option value="8">Mobile Internet</option></select></div></div><!--Show only if transaction type is device--><div class="form-group show-if-device"><div class="col-sm-2 text-right transaction"><span class="transaction">Warranty Type:</span></div><div class="col-sm-10"><select required="" id="warrantyDropdown" name="warrantyDropdown"><option disabled="" selected="" value="0" class="transact">Select Warranty Type</option><option value="1">Device Care</option><option value="2">Device Care &amp; T-UP</option><option value="3">AppleCare+</option><option value="4">AppleCare+ &amp; T-UP</option></select></div></div><!--Show only on all transaction types, but Accessory--><div class="form-group show-if-not-accessory"><div class="col-sm-2 text-right transaction"><span class="transaction">Attached?</span></div><div class="col-sm-10"><select required="" id="attached" name="attachedDropdown" class="transact"><option selected="" value="no">No</option><option value="yes">Yes</option></select></div></div><!--Show on all transaction types--><div class="form-group show-on-all"><div class="col-sm-2 text-right transaction"><span class="transaction">Number of Accessories:</div></span><div class="col-sm-10"><input type="number" name="accessoryCount" value="0" min="0" max="100" class="form-control input-number"></div></div></div>';
+    var deviceFields = '<div class="single-device">\
+                            <h3 class="device show-if-device">Device 1</h3>\
+                                <div class="form-group show-if-device">\
+                                    <div class="col-sm-2 text-right transaction"><span class="transaction">Activation Type:</span></div>\
+                                        <div class="col-sm-10">\
+                                            <select required="" id="activationDropdown" name="activationDropdown" class="transact">\
+                                                <option disabled="" selected="" value="0">Select Activation Type</option>\
+                                                <option value="1">New Activation</option>\
+                                                <option value="2">Renewal</option>\
+                                                <option value="3">Prepaid</option>\
+                                                <option value="4">Outright</option>\
+                                            </select>\
+                                        <div class="sbs"><input type="checkbox" name="sbsActivation"><span>SBS Activation?</span></div>\
+                                    </div>\
+                                </div>\
+                        <!--Show only if transaction type is device-->\
+                        <div class="form-group show-if-device">\
+                            <div class="col-sm-2 text-right transaction"><span class="transaction">Device Type:</span></div>\
+                            <div class="col-sm-10">\
+                                <select required="" id="deviceDropdown" name="deviceDropdown" class="transact">\
+                                    <option disabled="" selected="" value="0">Select Device Type</option>\
+                                    <option value="1">iPhone</option>\
+                                    <option value="2">Android</option>\
+                                    <option value="3">Blackberry</option>\
+                                    <option value="4">Other Device</option>\
+                                    <option value="5">SIM</option>\
+                                    <option value="6">Tablet</option>\
+                                    <option value="7">iPad</option>\
+                                    <option value="8">Mobile Internet</option>\
+                                </select>\
+                            </div>\
+                        </div>\
+                        <!--Show only if transaction type is device-->\
+                        <div class="form-group show-if-device">\
+                            <div class="col-sm-2 text-right transaction"><span class="transaction">Warranty Type:</span></div>\
+                            <div class="col-sm-10">\
+                                <select required="" id="warrantyDropdown" name="warrantyDropdown" class="warrantyDropdown">\
+                                    <option disabled="" selected="" value="0" class="transact">Select Warranty Type</option>\
+                                    <option value="1">Device Care</option>\
+                                    <option value="2">Device Care &amp; T-UP</option>\
+                                    <option value="3">AppleCare+</option>\
+                                    <option value="4">AppleCare+ &amp; T-UP</option>\
+                                </select>\
+                            </div>\
+                        </div>\
+                        <!--Show only on all transaction types, but Accessory-->\
+                        <div class="form-group show-if-not-accessory">\
+                            <div class="col-sm-2 text-right transaction"><span class="transaction">Attached?</span></div>\
+                            <div class="col-sm-10">\
+                                <select required="" id="attached" name="attachedDropdown" class="transact">\
+                                    <option selected="" value="no">No</option>\
+                                    <option value="yes">Yes</option>\
+                                </select>\
+                            </div>\
+                        </div>\
+                        <!--Show on all transaction types-->\
+                        <div class="form-group show-on-all">\
+                            <div class="col-sm-2 text-right transaction"><span class="transaction">Number of Accessories:</div></span>\
+                                <div class="col-sm-10">\
+                                    <input type="number" name="accessoryCount" value="0" min="0" max="100" class="form-control input-number">\
+                                </div>\
+                            </div>\
+                        </div>';
     $('.device-group').html(deviceFields);
     count = 2;
 
 }
 
 
-
 $("#transactionDropdown").change(function () {
     var selector = document.getElementById('transactionDropdown');
     if (selector.value == "1") {
-        $('.show-if-device').attr('style','display: block');
+        $('.show-if-device').attr('style', 'display: block');
     }
     else if (selector.value == "2") {
         $('.show-if-not-accessory').attr('style', 'display: none');
-        $('.show-if-device').attr('style','display: none');
+        $('.show-if-device').attr('style', 'display: none');
     }
     if (selector.value != "1") {
         $('.show-if-device').attr('style', 'display: none');
@@ -111,7 +237,7 @@ $("#transactionDropdown").change(function () {
     if (selector.value != "2") {
         $('.show-if-not-accessory').attr('style', 'display: block');
     }
-        $('.show-on-all').attr('style','display: block');
+    $('.show-on-all').attr('style', 'display: block');
 });
 
 
