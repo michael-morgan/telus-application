e4d9336a148d4252d3eb56dc4cb09608b6d5689f
j-whiteside <whiteside.jeremy@gmail.com>
2016-07-14 17:30:09 -0400

    Delete confirmation when deleting users

---
+++ build/public/javascripts/modify-users.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Jeremy on 7/14/2016.
+ */
+
+function deleteUser() {
+    //Confirmation modal that appears when a user attempts to delete another user from the modify users page
+
+        bootbox.confirm("Are you sure you want to delete this user?", function(result) {
+            if (result == true) {
+                //If the user clicks on "OK" in the confirmation box
+                document.getElementById("listUsersForm").submit();
+            }
+        });
+}
