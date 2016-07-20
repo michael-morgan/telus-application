e916e9b307f4433a1d64d823c292e109c4b03952
Bradley Haveman <bradley@haveman.ca>
2016-07-20 01:18:00 -0400

    Added smooth scrolling for id links on the documentation page

--- build/public/javascripts/documentation.js
+++ build/public/javascripts/documentation.js
@@ -17,4 +17,14 @@ $(document).ready(function(){
 
     $('#back-to-top').tooltip('show');
 
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+
 });
